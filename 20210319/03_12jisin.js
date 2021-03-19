42b3411c5871063719735b9455f5d19210a12ce3
dltmdwlr <dltmdwlr@gmail.com>
2021-03-19 11:52:12 +0900

    커밋

---
+++ 20210319/03_12jisin.js
@@ -0,0 +1,43 @@
+const thee = 1996 % 12;
+
+switch(thee){
+    case 0:
+        console.log('신')
+        break
+    case 1:
+        console.log('유')
+        break
+    case 2:
+        console.log('술')
+        break
+    case 3:
+        console.log('해')
+        break
+    case 4:
+        console.log('자')
+        break
+    case 5:
+        console.log('축')
+        break
+    case 6:
+        console.log('인')
+        break
+    case 7:
+        console.log('묘')
+        break
+    case 8:
+        console.log('진')
+        break
+    case 9:
+        console.log('사')
+        break
+    case 10:
+        console.log('오')
+        break
+    case 11:
+        console.log('미')
+        break
+    default:
+        console.log('존재 X')
+        break
+}
