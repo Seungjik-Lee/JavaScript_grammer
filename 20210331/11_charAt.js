05c2e957b16aa8e82938a1a4c8152d973f0346f9
dltmdwlr <dltmdwlr@gmail.com>
2021-04-01 11:51:12 +0900

    charAt

---
+++ 20210331/11_charAt.js
@@ -0,0 +1,8 @@
+let example = "이승직"
+let one1 = example.charAt(0)
+let one2 = example.charAt(1)
+let one3 = example.charAt(2)
+console.log(example)
+console.log(one1)
+console.log(one2)
+console.log(one3)
