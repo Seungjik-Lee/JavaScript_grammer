8f341982eab1ceaec4a96d4bc39d8708b677e7fc
dltmdwlr <dltmdwlr@gmail.com>
2021-04-01 10:25:51 +0900

    문제

--- 20210331/11_Prob4.js
+++ 20210331/11_Prob4.js
@@ -1,9 +1,8 @@
 let output = ''
-for(let i = 1; i < 15; i++)
-{
-    for(let j = 15; j >i; j--)
+for (let i = 1; i < 15; i++) {
+    for (let j = 15; j > i; j--)
         output += ' '
-    for (let k =0; k < 2*i-1; k++)
+    for (let k = 0; k < 2 * i - 1; k++)
         output += '*'
 
     output += '\n'
